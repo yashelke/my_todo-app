1394dfc27a5fdf5cfdfe8b0cee735be90f4e0189
Yash Shelke <yashshelke446@gmail.com>
2025-08-18 17:01:58 +0530

    Fix module loading issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-// import './App.css'
+
 import Todo from './components/Todo'
 import LandingPage from './components/LandingPage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
