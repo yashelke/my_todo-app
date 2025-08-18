36c511f385a3ae70045a19e59f0a42b1f0bd3c74
Yash Shelke <yashshelke446@gmail.com>
2025-08-18 17:06:27 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
-import Todo from './components/Todo'
-import LandingPage from './components/LandingPage'
+import Todo from './components/Todo.jsx'
+import LandingPage from './components/LandingPage.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
   
