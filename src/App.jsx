e061837f04ff2b206d5d84d3bf4fd5734985079d
Yash Shelke <yashshelke446@gmail.com>
2025-08-18 16:37:03 +0530

    Refreshment problem of Todo.jsx page added React Router to handle SPA Routing

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <BrowserRouter>
     <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/todo" element={<Todo />} />
+        <Route path="*" element={<Todo />} />
     </Routes>
     
     
