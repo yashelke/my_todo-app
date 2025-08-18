53d20f4c67bd70a8a73564455dd24084593c53ec
Yash Shelke <yashshelke446@gmail.com>
2025-08-18 16:46:07 +0530

    Added vercel.json to handle SPA Routing...second try

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,8 @@ function App() {
     <BrowserRouter>
     <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="*" element={<Todo />} />
+        <Route path="/todo" element={<Todo />} />
+        <Route path="*" element={<Todo />} /> {/* Catch-all route */}
     </Routes>
     
     
